a81de666a27afceac8c49c2ebcf684db97bf4da9
Nicholas Dela Torre <nicholas.delatorre@gmail.com>
2018-12-12 15:29:27 -0200

    Inclusao AuthGuard e Botao Logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '', component: HomeComponent, canActivate: [AuthGuard] },
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent}
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './_service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private authService: AuthService) {}
+
+  sair() {
+    if (confirm('Deseja realmente sair?')) {
+      this.authService.logout();
+    }
+  }
 }
